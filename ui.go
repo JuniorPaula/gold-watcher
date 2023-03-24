52830e593876cb4ff69299cc4a07e20b7b576952
Junior Paula <luke.junnior@icloud.com>
2023-03-24 19:10:16 -0300

    Display the chart on app window

--- ui.go
+++ ui.go
@@ -20,8 +20,10 @@ func (app *Config) makeUI() {
 	toolBar := app.getToolBar()
 	app.ToolBar = toolBar
 
+	priceTabContent := app.pricesTab()
+
 	tabs := container.NewAppTabs(
-		container.NewTabItemWithIcon("Prices", theme.HomeIcon(), canvas.NewText("Price content goes here", nil)),
+		container.NewTabItemWithIcon("Prices", theme.HomeIcon(), priceTabContent),
 		container.NewTabItemWithIcon("Holdings", theme.InfoIcon(), canvas.NewText("Holdings content goes here", nil)),
 	)
 	tabs.SetTabLocation(container.TabLocationTop)
