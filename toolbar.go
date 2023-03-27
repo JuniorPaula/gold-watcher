d776edb0654ce8b9e246b9c6d1d34097ea2edd5b
Junior Paula <luke.junnior@icloud.com>
2023-03-27 20:01:40 -0300

    Enable the add holdings button in the toolbar

--- toolbar.go
+++ toolbar.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
@@ -8,7 +10,9 @@ import (
 func (app *Config) getToolBar() *widget.Toolbar {
 	toolBar := widget.NewToolbar(
 		widget.NewToolbarSpacer(),
-		widget.NewToolbarAction(theme.DocumentCreateIcon(), func() {}),
+		widget.NewToolbarAction(theme.DocumentCreateIcon(), func() {
+			app.addHoldingsDialog()
+		}),
 		widget.NewToolbarAction(theme.ViewRefreshIcon(), func() {
 			app.refreshPriceContent()
 		}),
@@ -16,4 +20,37 @@ func (app *Config) getToolBar() *widget.Toolbar {
 	)
 
 	return toolBar
+}
+
+func (app *Config) addHoldingsDialog() dialog.Dialog {
+	addAmountEntry := widget.NewEntry()
+	purchaseDateEntry := widget.NewEntry()
+	purchasePriceEntry := widget.NewEntry()
+
+	app.AddHoldingsPurchseAmountEntry = addAmountEntry
+	app.AddHoldingsPurchseDateEntry = purchaseDateEntry
+	app.AddHoldingsPurchsePriceEntry = purchasePriceEntry
+
+	purchaseDateEntry.PlaceHolder = "YYYY-MM-DD"
+
+	addForm := dialog.NewForm(
+		"Add Gold",
+		"Add",
+		"Cancel",
+		[]*widget.FormItem{
+			{Text: "Amount in toz", Widget: addAmountEntry},
+			{Text: "Purchase Price", Widget: purchasePriceEntry},
+			{Text: "Purchase Date", Widget: purchaseDateEntry},
+		},
+		func(valid bool) {
+			if valid {
+				//
+			}
+		},
+		app.MainWindow)
+
+	addForm.Resize(fyne.Size{Width: 400})
+	addForm.Show()
+
+	return addForm
 }
