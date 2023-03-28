39bcff17c95d9989b1b86a88068ff80829dfdd07
Junior Paula <luke.junnior@icloud.com>
2023-03-28 07:53:18 -0300

    Test from AddHoldings dialogs

--- toolbar_test.go
+++ toolbar_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"fyne.io/fyne/v2/test"
+)
 
 func TestApp_ToolBar(t *testing.T) {
 	tb := testApp.getToolBar()
@@ -8,4 +12,25 @@ func TestApp_ToolBar(t *testing.T) {
 	if len(tb.Items) != 4 {
 		t.Error("wrong number of items in toobar")
 	}
+}
+
+func TestApp_addHoldingsDialogs(t *testing.T) {
+	testApp.addHoldingsDialog()
+
+	test.Type(testApp.AddHoldingsPurchseAmountEntry, "1")
+	test.Type(testApp.AddHoldingsPurchsePriceEntry, "1000")
+	test.Type(testApp.AddHoldingsPurchseDateEntry, "2023-03-28")
+
+	if testApp.AddHoldingsPurchseDateEntry.Text != "2023-03-28" {
+		t.Error("date not correct")
+	}
+
+	if testApp.AddHoldingsPurchseAmountEntry.Text != "1" {
+		t.Error("amount not correct")
+	}
+
+	if testApp.AddHoldingsPurchsePriceEntry.Text != "1000" {
+		t.Error("price not correct")
+	}
+
 }
